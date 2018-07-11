32edce7747bdf75dc219906e44490238a7840316
mmacdonald1 <melmacd94@gmail.com>
2018-07-11 10:04:43 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+let index = 0;
+  document.body.addEventListener('keydown',(e) => {
+    const keyName = e.key
+    console.log('keydown event\n\n' + 'key: ' + keyName);
+    console.log(index)
+    console.log(codes[index])
+    console.log(codes.length-1)
+
+    if(keyName === codes[index]){
+
+        if(index === codes.length-1){
+          window.alert("Hurray!")
+          console.log('hellooooooooo')
+           index = 0;
+        }
+          index++
+    }
+    else {
+      console.log("booo")
+      index= 0;
+    }
+
+
+  })
 }
